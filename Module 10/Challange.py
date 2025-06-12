caa9ca365a811fab81f228e2d45fdf33cd729a2e
dinaa <elmedinabucolli1612@gmail.com>
2025-06-12 17:17:11 +0200

    challange finished
    challange finished

--- Module 10/Challange.py
+++ Module 10/Challange.py
@@ -1,3 +1,4 @@
+#create a class called DigitalSchool with 4 atributes a constructor, getters and setters with decorator and two methosed
 class DigitalSchool:
     def __init__(self,name,city,state,courses):
         self.__name=name
@@ -5,87 +6,80 @@ class DigitalSchool:
         self.__state=state
         self.__courses=courses
 
+    #def get and set for name
     @property
     def name(self):
         return self.__name
 
-    @name.settter
+    @name.setter
     def name(self, name):
         self.__name = name
-
+    #def get and set for city
     @property
     def city(self):
         return self.__city
 
-    @city.settter
+    @city.setter
     def city(self, city):
         self.__city = city
-
+    #def get and set for state
     @property
     def state(self):
         return self.__state
 
-    @state.settter
+    @state.setter
     def state(self, state):
         self.__state = state
-
+    #def get and set for courses
     @property
     def courses(self):
         return self.__courses
 
-    @courses.settter
+    @courses.setter
     def courses(self, courses):
         self.__courses = courses
 
-    def show_school_info(self):pass #return a dictionary with school info
-    def organize_hackathon(self):pass #define a placeholder method to be overriden by a subclasses
+    #def methods
+    def show_school_info(self):
+        return {
+            "name":self.__name,
+            "city":self.__city,
+            "state":self.__state,
+            "courses":self.__courses
+        }   #return a dictionary with school info
 
+    def organize_hackathon(self): #define a placeholder method to be overriden by a subclasses
+        print("Organizing a generic hackathon.")
 
+#define a subclass called DS_Prishtina where it has a private atribute called stydent number and two methods organize_hacakathon and SCF
 class DS_Prishtina(DigitalSchool):
     def __init__(self,name,city,state,courses,student_number):
-        @property
-        def name(self):
-            return self.__name
+        super().__init__(name, city, state, courses)
+        self.__student_number=student_number
 
-        @name.settter
-        def name(self, name):
-            self.__name = name
+    def SCF(self):
+        print("First edition")
 
-        @property
-        def city(self):
-            return self.__city
+    def organize_hackathon(self):
+        print("We are doing a data analysis hackathon")
 
-        @city.settter
-        def city(self, city):
-            self.__city = city
+prishtina_obj=DS_Prishtina("DS_Prishtina","prishtina","Kosove",["PHP","Python","Java"], 3000)
+prishtina_obj.SCF()
+prishtina_obj.organize_hackathon()
+print(prishtina_obj.show_school_info())
 
-        @property
-        def state(self):
-            return self.__state
+class DS_Ferizaj(DigitalSchool):
+    def __init__(self,name,city,state,courses,student_number):
+        super().__init__(name, city, state, courses)
+        self.__student_number=student_number
 
-        @state.settter
-        def state(self, state):
-            self.__state = state
+    def organize_hackathon(self):
+        print("We are doing a 48 hour hackathon. If you win you get an internship")
 
-        @property
-        def courses(self):
-            return self.__courses
+    def internship(self):
+        print("You got an internship!")
 
-        @courses.settter
-        def courses(self, courses):
-            self.__courses = courses
-
-        @property
-        def student_number(self):
-            return self.__student_number
-
-        @student_number.settter
-        def student_number(self, student_number):
-            self.__student_number = student_number
-
-        def SCF(self):
-            print("Spring Code")
-
-        def organize_hackathon(self): pass #overrride the method
-
-        #REDO
+ferizaj_obj = DS_Ferizaj("DS_Ferizaj","ferizaj","Kosove",["PHP","React","Java"],4444)
+ferizaj_obj.organize_hackathon()
+ferizaj_obj.internship()
+print(ferizaj_obj.show_school_info())
