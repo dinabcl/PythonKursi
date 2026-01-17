af389292417bd5e89b195b57a61cf1e0d7dcecec
dinaa <elmedinabucolli1612@gmail.com>
2026-01-17 14:44:52 +0100

    Module19

--- Module19/adding_data.py
+++ Module19/adding_data.py
@@ -1,17 +1,39 @@
-from itertools import count
-
-import streamlit as st
 import pandas as pd
+import streamlit as st
 import plotly.express as px
-from mod8 import color
-from streamlit import title
 
-books_df = pd.read_csv("bestsellers_with_categories_2022_03_27.csv")
+books_df = pd.read_csv('bestsellers_with_categories_2022_03_27.csv')
 
 st.title("Bestselling Books Analysis")
-st.write("This app analyzes the Amazon Top Selling Books from 2009 to 2022.")
+st.write('This app analyzes the Amazon Top Selling Books from 2009-2022')
 
-st.subheader("Summary Statistics")
+st.sidebar.header("Add New Book Data")
+with st.sidebar.form('Book_form'):
+    new_name = st.text_input("Book Name")
+    new_author = st.text_input("Author")
+    new_user_rating = st.slider("User Rating", 0.0, 0.5, 0.0, 0.1)
+    new_reviews = st.number_input("Reviews", min_value=0, step=1)
+    mew_price = st.number_input("Price", min_value=0, step=1)
+    new_year = st.number_input("Year", min_value=2009, max_value=2022, step=1)
+    new_genre = st.selectbox("Genre", books_df['Genre'].unique())
+    submit_button = st.form_submit_button(label="Add Book")
+
+if submit_button:
+    new_data = {
+        'Name': new_name,
+        'Author': new_author,
+        'User Rating': new_user_rating,
+        'Reviews': new_reviews,
+        'Price': mew_price,
+        'Year':new_year,
+        'Genre':new_genre,
+    }
+
+    books_df = pd.concat([pd.DataFrame(new_data, index=[0]), books_df], ignore_index=True)
+    books_df.to_csv('bestsellers_with_categories_2022_03_27.csv', index=False)
+    st.sidebar.success("New book added successfully!")
+
+st.subheader('Summery Statistics')
 total_books = books_df.shape[0]
 unique_titles = books_df['Name'].nunique()
 average_rating = books_df['User Rating'].mean()
@@ -21,17 +43,16 @@ col1, col2, col3, col4 = st.columns(4)
 col1.metric("Total Books", total_books)
 col2.metric("Unique Titles", unique_titles)
 col3.metric("Average Rating", f'{average_rating:.2f}')
-col4.metric("Average Price", f'${average_price:.2f}')
+col4.metric("Average Price",f'${average_price:.2f}')
 
 st.subheader("Dataset Preview")
 st.write(books_df.head())
 
 col1, col2 = st.columns(2)
-
-with col1:
-    st.subheader("Top 10 book titeles")
-    top_titles = books_df['Name'].value_counts().head(10)
-    st.bar_chart(top_titles)
+with col2:
+    st.subheader("Top 10 Authors")
+    top_authors = books_df['Author'].value_counts().head(10)
+    st.bar_chart(top_authors)
 
 with col2:
     st.subheader("Top 10 Authors")
